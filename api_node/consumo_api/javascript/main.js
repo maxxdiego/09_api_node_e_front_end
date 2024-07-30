f4878876729a9328e7a8ea6cc218fc0ef3665923
maxx.diego <77diego.max@gmail.com>
2024-07-30 17:12:03 -0300

    @maxxdiego

--- api_node/consumo_api/javascript/main.js
+++ api_node/consumo_api/javascript/main.js
@@ -5,72 +5,71 @@ const axiosConfig = {
   },
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Capturando o botão de cadastrar
-  const createBtn = document.getElementById("createBtn");
-  // Escuta ao evento click no botão
-  createBtn.addEventListener("click", createGame);
-  // Capturando o botão de alterar
-  const updateBtn = document.getElementById("updateBtn");
-  // Escuta ao evento click no botão de alterar
-  updateBtn.addEventListener("click", updateGame);
+// Capturando o botão de cadastrar
+const createBtn = document.getElementById("createBtn");
+// Escuta ao evento click no botão
+createBtn.addEventListener("click", createGame);
+// Capturando o botão de alterar
+const updateBtn = document.getElementById("updateBtn");
+// Escuta ao evento click no botão de alterar
+updateBtn.addEventListener("click", updateGame);
 
-  // CADASTRO
-  // Função para CADASTRAR games
-  async function createGame() {
-    try {
-      const form = document.getElementById("createForm");
-      form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Evita o envio padrão do formulário
-      });
+// CADASTRO
+// Função para CADASTRAR games
+async function createGame() {
+  try {
+    const form = document.getElementById("createForm");
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Evita o envio padrão do formulário
+    });
 
-      const titleInput = document.getElementById("title");
-      const platformInput = document.getElementById("platform");
-      const yearInput = document.getElementById("year");
-      const priceInput = document.getElementById("price");
+    const titleInput = document.getElementById("title");
+    const platformInput = document.getElementById("platform");
+    const yearInput = document.getElementById("year");
+    const priceInput = document.getElementById("price");
 
-      const game = {
-        title: titleInput.value,
-        platform: platformInput.value,
-        year: yearInput.value,
-        price: priceInput.value,
-      };
+    const game = {
+      title: titleInput.value,
+      platform: platformInput.value,
+      year: yearInput.value,
+      price: priceInput.value,
+    };
 
-      const response = await axios.post(
-        "https://09-api-node.vercel.app/game",
-        game,
-        axiosConfig
-      );
-      if (response.status == 201) {
-        alert("Game cadastrado!");
-        location.href = "home.html";
-      }
-    } catch (error) {
-      console.log(err);
+    const response = await axios.post(
+      "https://09-api-node.vercel.app/game",
+      game,
+      axiosConfig
+    );
+    if (response.status == 201) {
+      alert("Game cadastrado!");
+      location.href = "home.html";
     }
+  } catch (error) {
+    console.log(err);
   }
+}
 
-  // LISTANDO OS JOGOS
-  async function getGames() {
-    try {
-      const response = await axios.get(
-        "https://09-api-node.vercel.app/games",
-        axiosConfig
-      );
-      const games = response.data.games;
-      const listGames = document.getElementById("games");
+// LISTANDO OS JOGOS
+async function getGames() {
+  try {
+    const response = await axios.get(
+      "https://09-api-node.vercel.app/games",
+      axiosConfig
+    );
+    const games = response.data.games;
+    const listGames = document.getElementById("games");
 
-      games.forEach((game) => {
-        let item = document.createElement("ul");
+    games.forEach((game) => {
+      let item = document.createElement("ul");
 
-        // Setando os atributos ID, título, price e descrição para cada game
-        item.setAttribute("data-id", game._id);
-        item.setAttribute("data-title", game.title);
-        item.setAttribute("data-platform", game.platform);
-        item.setAttribute("data-year", game.year);
-        item.setAttribute("data-price", game.price);
+      // Setando os atributos ID, título, price e descrição para cada game
+      item.setAttribute("data-id", game._id);
+      item.setAttribute("data-title", game.title);
+      item.setAttribute("data-platform", game.platform);
+      item.setAttribute("data-year", game.year);
+      item.setAttribute("data-price", game.price);
 
-        item.innerHTML = `<img src="images/game_cd_cover.png" alt="Jogo em estoque">
+      item.innerHTML = `<img src="images/game_cd_cover.png" alt="Jogo em estoque">
       <h3>${game.title}</h3>
         <li>Plataforma: ${game.platform}</li> 
         <li>Ano: ${game.year}</li> 
@@ -79,41 +78,40 @@ document.addEventListener("DOMContentLoaded", function () {
           currency: "BRL",
         })}</li>`;
 
-        var deleteBtn = document.createElement("button");
-        deleteBtn.innerHTML = "Deletar";
-        deleteBtn.classList.add("btnDel");
-        deleteBtn.addEventListener("click", () => {
-          deleteGame(item);
-        });
-
-        var editBtn = document.createElement("button");
-        editBtn.innerHTML = "Editar";
-        editBtn.classList.add("btnEdit");
-        editBtn.addEventListener("click", () => {
-          var modal = document.querySelector(".editModal");
-          modal.style.display = "block";
-          var span = document.querySelector(".modalClose");
-          span.onclick = () => {
-            modal.style.display = "none";
-          };
-          window.onclick = (event) => {
-            if (event.target == modal) {
-              modal.style.display = "none";
-            }
-          };
-          loadForm(item);
-        });
-
-        item.appendChild(deleteBtn);
-        item.appendChild(editBtn);
-        listGames.appendChild(item);
+      var deleteBtn = document.createElement("button");
+      deleteBtn.innerHTML = "Deletar";
+      deleteBtn.classList.add("btnDel");
+      deleteBtn.addEventListener("click", () => {
+        deleteGame(item);
       });
-    } catch (error) {
-      console.log(error);
-    }
+
+      var editBtn = document.createElement("button");
+      editBtn.innerHTML = "Editar";
+      editBtn.classList.add("btnEdit");
+      editBtn.addEventListener("click", () => {
+        var modal = document.querySelector(".editModal");
+        modal.style.display = "block";
+        var span = document.querySelector(".modalClose");
+        span.onclick = () => {
+          modal.style.display = "none";
+        };
+        window.onclick = (event) => {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        };
+        loadForm(item);
+      });
+
+      item.appendChild(deleteBtn);
+      item.appendChild(editBtn);
+      listGames.appendChild(item);
+    });
+  } catch (error) {
+    console.log(error);
   }
-  getGames()
-});
+}
+getGames();
 
 // EXCLUSÃO
 
