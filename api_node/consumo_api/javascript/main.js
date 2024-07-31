8ff26530f9b50319e2e76d16a2b28ad93711dc41
maxx.diego <77diego.max@gmail.com>
2024-07-31 17:57:34 -0300

    @maxxdiego

--- api_node/consumo_api/javascript/main.js
+++ api_node/consumo_api/javascript/main.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
           })}</li>`;
 
         var deleteBtn = document.createElement("button");
-        deleteBtn.innerHTML = "X";
+        deleteBtn.innerHTML = "Excluir";
         deleteBtn.classList.add("btnDel");
         deleteBtn.addEventListener("click", () => {
           deleteGame(item);
